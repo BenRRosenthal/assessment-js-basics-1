c7d902562b6db4096d0397ee5fa95345b3a125c3
BenRRosenthal <benrrosenthal@gmail.com>
2022-07-10 19:17:31 -0600

    Finished and completed sdlc.js

--- sdlc.js
+++ sdlc.js
@@ -18,4 +18,12 @@
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
 
-const analysisOfRequirments = "This is the second step, this can be considered part of the planning stage. You will need"
+let analysisOfRequirments = "This is the second step, this can be considered part of the planning stage. You will need to now make sure that you know what is required for the project you planned."
+
+var design = "This is one of the hearts for the process in the sense that you are going to decide what languages you will be using, how the customer/user will use it and how it will look to them, as well as where the program run (Apple, Andriod, etc.) . In a way you could say this is where pencil hits the canvas before you start actually painting."
+
+const implementation = "This it is where we finally get to code, this is what the first 3 steps set up. Now you get to put paint on the canvas as well as commit the project to maybe a test iteration."
+
+let testingAndIntegration = "This is where bugs are found on the user side as well as where we push our program to the public connecting the program to the needed datatbases and stores/websites."
+
+var maintain = "This is sometimes the hardest or easiest depending on how well you do in the previous steps. You will be making fixes on small bugs but maybe just restarting the SDLC all over again to just keep redoing/updating your program/code."
