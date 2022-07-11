02e804965c928cddd52ce8dddcaf143c09934673
BenRRosenthal <benrrosenthal@gmail.com>
2022-07-10 20:56:37 -0600

    Tried ASCI and just can noth get it to work

--- password.js
+++ password.js
@@ -3,7 +3,7 @@ let password = "Are Y0u Happy N0w!"
 
 
 if(password.length >= 10 && /\d/.test(password) === true && password.length <= 24){
-    console.log('Password is Valid!')
+    console.log(`Password is Valid!`)
     //Added condition to be if password is more than length 24 not valid.
 } else if(password.length >= 24){
     console.log('Password Length exceeds 24 charaters')
